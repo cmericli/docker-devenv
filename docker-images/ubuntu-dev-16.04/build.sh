756025150d44a4ae913f66e351984cd635f75d19
Cetin Mericli <cmericli@nrec.ri.cmu.edu>
2017-12-12 11:36:01 -0500

    with updates

--- docker-images/ubuntu-dev-16.04/build.sh
+++ docker-images/ubuntu-dev-16.04/build.sh
@@ -2,9 +2,4 @@
 
 echo "detected host UID=$UID"
 
-if [ "$#" -ne 1 ] 
-then 
-	docker build --no-cache --build-arg CUSTOM_UID=$UID -t ubuntu-dev:16.04 .
-else
-	docker --no-cache --build-arg CUSTOM_UID=$UID build -t $1 .
-fi
+docker build --no-cache --build-arg CUSTOM_UID=$UID -t ubuntu-dev:16.04 .
